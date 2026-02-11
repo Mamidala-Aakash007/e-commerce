8bb5ba3d2547356a1938c67a0be2bcf80386b048
Mamidala-Aakash007 <luckyaakashavi1@gmail.com>
2026-02-11 22:25:01 +0530

    add Price and price formatter

--- components/AddToWishlistButton.tsx
+++ components/AddToWishlistButton.tsx
@@ -1,7 +1,21 @@
+import { cn } from "@/lib/utils";
 import { Product } from "@/sanity.types";
+import { Heart } from "lucide-react";
 
-const AddToWishlistButton = ({ product }: { product: Product }) => {
-  return <div>AddToWishlistButton</div>;
+const AddToWishlistButton = ({
+  product,
+  className,
+}: {
+  product: Product;
+  className?: string;
+}) => {
+  return (
+    <div className={cn(`absolute top-2 right-0`, className)}>
+      <div className={`p-2.5 rounded-full hover:bg-shop_dark_green hover:text-white hoverEffect bg-shop_light_pink`}>
+        <Heart size={15}/>
+      </div>
+    </div>
+  );
 };
 
 export default AddToWishlistButton;
