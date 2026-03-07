651c7fbada638e9a33d220c25379656c7fdbc547
Mamidala-Aakash007 <luckyaakashavi1@gmail.com>
2026-03-07 20:53:45 +0530

    add analytics

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: {
